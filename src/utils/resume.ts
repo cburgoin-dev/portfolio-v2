83fa8470b162757b66fea1846df3ee2c304d7f14
cburgoin-dev <cristian.burgoin.dev@gmail.com>
2026-06-24 17:15:26 -0700

    docs: finalize professional resume update

--- src/utils/resume.ts
+++ src/utils/resume.ts
@@ -1,5 +1,5 @@
-import ResumeEN from "../assets/resume/CristianBurgoin_Resume_EN.pdf";
-import ResumeES from "../assets/resume/CristianBurgoin_CV_ES.pdf";
+import ResumeEN from "../assets/resume/Cristian_Burgoin_Software_Engineer_Resume.pdf";
+import ResumeES from "../assets/resume/Cristian_Burgoin_Ingeniero_de_Software_CV.pdf";
 
 export const resumes = {
     en: ResumeEN,
